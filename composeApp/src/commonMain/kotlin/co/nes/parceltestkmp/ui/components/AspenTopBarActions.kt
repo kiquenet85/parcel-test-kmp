7f37f4830526abf0ddba1f8e650a98a071cecdf0
Nes <kiquenet85@gmail.com>
2024-03-26 23:41:47 -0500

    * Implement top bar with back for detail screen * Implement Top Bar layout

--- composeApp/src/commonMain/kotlin/co/nes/parceltestkmp/ui/components/AspenTopBarActions.kt
+++ composeApp/src/commonMain/kotlin/co/nes/parceltestkmp/ui/components/AspenTopBarActions.kt
@@ -1,5 +1,6 @@
 package co.nes.parceltestkmp.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -8,7 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.LocationOn
@@ -18,66 +21,92 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import co.nes.parceltestkmp.ui.theme.AspenTheme
 
 @Composable
 fun AspenTopBarActions(
     modifier: Modifier = Modifier,
-    items: List<String>,
-    currentSelection: String,
+    layout: AspenTopBarLayout = AspenTopBarLayout.Home,
     onClick: (String) -> Unit,
+    items: List<String>? = null,
+    currentSelection: String? = null,
     onDismissRequest: () -> Unit = {},
 ) {
-    val expanded = remember { mutableStateOf(false) }
+    when(layout) {
+        AspenTopBarLayout.Home -> {
+            val expanded = remember { mutableStateOf(false) }
+            val currentSelectionHome = currentSelection ?: ""
+            val itemsHome = items ?: emptyList()
 
-    Box(
-        modifier = modifier
-            .wrapContentSize(Alignment.TopEnd)
-            .padding(end = 16.dp)
-            .clickable {
-                expanded.value = true
+            Box(
+                modifier = modifier
+                    .wrapContentSize(Alignment.TopEnd)
+                    .padding(end = 16.dp)
+                    .clickable {
+                        expanded.value = true
+                    }
+            ) {
+                Row {
+                    Icon(
+                        modifier = Modifier
+                            .height(15.dp)
+                            .width(15.dp),
+                        imageVector = Icons.Default.LocationOn,
+                        contentDescription = "Place",
+                        tint = AspenTheme.colors.primary,
+                    )
+                    AspenText(
+                        text = currentSelectionHome,
+                        modifier = Modifier
+                            .padding(horizontal = 6.dp)
+                            .height(15.dp)
+                            .widthIn(min = 64.dp),
+                        style = AspenTheme.typography.bodySmall,
+                        color = AspenTheme.colors.onSecondary,
+                    )
+                    Icon(
+                        imageVector = if(expanded.value) Icons.Default.KeyboardArrowUp
+                        else Icons.Default.KeyboardArrowDown,
+                        contentDescription = "Dropdown",
+                        modifier = Modifier
+                            .height(15.dp)
+                            .width(15.dp),
+                        tint = AspenTheme.colors.primary,
+                    )
+                }
+                AspenDropdownMenu(
+                    expanded = expanded.value,
+                    items = itemsHome,
+                    onClick = {
+                        onClick(it)
+                        expanded.value = false
+                    },
+                    onDismissRequest = {
+                        expanded.value = false
+                        onDismissRequest()
+                    },
+                )
             }
-    ) {
-        Row {
-            Icon(
-                modifier = Modifier
-                    .height(15.dp)
-                    .width(15.dp),
-                imageVector = Icons.Default.LocationOn,
-                contentDescription = "Place",
-                tint = AspenTheme.colors.primary,
-            )
-            AspenText(
-                text = currentSelection,
-                modifier = Modifier
-                    .padding(horizontal = 6.dp)
-                    .height(15.dp)
-                    .widthIn(min = 64.dp),
-                style = AspenTheme.typography.bodySmall,
-                color = AspenTheme.colors.onSecondary,
-            )
-            Icon(
-                imageVector = if(expanded.value) Icons.Default.KeyboardArrowUp
-                else Icons.Default.KeyboardArrowDown,
-                contentDescription = "Dropdown",
-                modifier = Modifier
-                    .height(15.dp)
-                    .width(15.dp),
-                tint = AspenTheme.colors.primary,
-            )
         }
-        AspenDropdownMenu(
-            expanded = expanded.value,
-            items = items,
-            onClick = {
-                onClick(it)
-                expanded.value = false
-            },
-            onDismissRequest = {
-                expanded.value = false
-                onDismissRequest()
-            },
-        )
+
+        AspenTopBarLayout.Detail -> {
+            Box(
+                modifier = modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .height(40.dp)
+                    .width(40.dp)
+                    .background(AspenTheme.colors.tertiary)
+                    .clickable { onClick("") },
+                contentAlignment = Alignment.Center,
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                    contentDescription = "Back",
+                    tint = AspenTheme.colors.secondary,
+                )
+            }
+        }
     }
 }
